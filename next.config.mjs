5481a4e33bde7260cae7e56ab5414537ccd37893
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-08-08 11:16:26 +1200

    trying

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    routes: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
