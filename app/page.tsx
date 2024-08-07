68f76abeb0d125ca7e36054598c32247466aca82
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-08-07 14:24:25 +1200

    For some reason worked on ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,30 @@ import { Button } from '@/components/ui/button';
 import axios from 'axios';
 import { User } from './models/user';
 import Header from '@/components/pageheader';
+import { Separator } from '@/components/ui/separator';
 
 async function GetUser(): Promise<User> {
   const response = await axios.get<User>('http://localhost:3000/api/v1/user/gettestuser');
-  return response.data
+  return response.data;
 }
 
 const Home = async () => {
-
-  const user: User = await GetUser()
+  const user: User = await GetUser();
 
   return (
-    <div className='grid gird-rows-2 h-full w-full'>
-      <Header><h1 className='flex flex-row gap-2'>Hello, <span className='text-primary'>{user.name}</span></h1></Header>
-      <div className='px-3 h-full w-full'>
-        <p>testing this will be deleted later, this is just a test, nothing to see here yet, this is just a test</p>
-        <div className='flex flex-row gap-4'>
-          <Button variant={'outline'}>This is one button</Button>
-          <Button variant={'secondary'}>This is also a button</Button>
-          <Button variant={'ghost'}>This is a third button</Button>
+    <div className='grid grid-rows-[auto,1fr] h-full w-full'>
+      <Header>
+        <h1 className='flex flex-row gap-2'>Hello, <span className='text-primary'>{user.name}</span></h1>
+      </Header>
+      <div className='p-4'>
+        <h1 className='text-foreground text-6xl font-bold'>Dashboard</h1>
+        <Separator />
+        <div>
+          <p>Other stuff</p>
         </div>
       </div>
-    </div>
-  )
+    </div >
+  );
 }
 
-export default Home
+export default Home;
